ef4465c7871aaaec62d055bb04cfe24c5bf55d68
maucaruso <carusojr@outlook.com>
2023-02-27 08:00:10 -0300

    chore: new db settings serverless

--- serverless.ts
+++ serverless.ts
@@ -1,14 +1,17 @@
 import type { AWS } from '@serverless/typescript';
 
-import hello from '@functions/hello';
-
 const serverlessConfiguration: AWS = {
-  service: 'todos-serverless-nodejs',
+  service: 'todo-app',
   frameworkVersion: '3',
-  plugins: ['serverless-esbuild'],
+  plugins: [
+    'serverless-esbuild',
+    "serverless-dynamodb-local",
+    'serverless-offline',
+  ],
   provider: {
     name: 'aws',
     runtime: 'nodejs14.x',
+    region: "us-east-1",
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
@@ -17,10 +20,48 @@ const serverlessConfiguration: AWS = {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
       NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
     },
+    iamRoleStatements: [
+      {
+        Effect: "Allow",
+        Action: ["dynamodb:*"],
+        Resource: ["*"]
+      },
+      {
+        Effect: "Allow",
+        Action: ["s3:*"],
+        Resource: ["*"],
+      }
+    ]
   },
-  // import the function via paths
-  functions: { hello },
-  package: { individually: true },
+  package: {
+    individually: false
+  },
+  functions: {
+    createTodo: {
+      handler: "src/functions/createTodo.handler",
+      events: [
+        {
+          http: {
+            path: "todos/{userid}",
+            method: "post",
+            cors: true,
+          }
+        }
+      ]
+    },
+    listTodo: {
+      handler: "src/functions/listTodo.handler",
+      events: [
+        {
+          http: {
+            path: "todos/{userid}",
+            method: "get",
+            cors: true,
+          }
+        }
+      ]
+    }
+  },
   custom: {
     esbuild: {
       bundle: true,
@@ -31,8 +72,43 @@ const serverlessConfiguration: AWS = {
       define: { 'require.resolve': undefined },
       platform: 'node',
       concurrency: 10,
+      external: []
     },
+    dynamodb: {
+      stages: ["dev", "local"],
+      start: {
+        port: 8000,
+        inMemory: true,
+        migrate: true,
+      }
+    }
   },
+  resources: {
+    Resources: {
+      dbCertificateUsers: {
+        Type: "AWS::DynamoDB::Table",
+        Properties: {
+          TableName: "todos",
+          ProvisionedThroughput: {
+            ReadCapacityUnits: 5,
+            WriteCapacityUnits: 5
+          },
+          AttributeDefinitions: [
+            {
+              AttributeName: "id",
+              AttributeType: "S",
+            }
+          ],
+          KeySchema: [
+            {
+              AttributeName: "id",
+              KeyType: "HASH"
+            }
+          ]
+        }
+      }
+    }
+  }
 };
 
 module.exports = serverlessConfiguration;
